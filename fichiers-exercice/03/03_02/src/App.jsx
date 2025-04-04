7d4ea49f5103bc3adf54a05ccc1615753c9f45e9
Sandy <sandy@Sandys-MBP-2.lan>
2025-04-04 15:52:47 +0200

    03_02

--- fichiers-exercice/03/03_02/src/App.jsx
+++ fichiers-exercice/03/03_02/src/App.jsx
@@ -3,19 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Header() {
+  return (
+  <>
+    <a href="https://vite.dev" target="_blank">
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+    </a>
+    <a href="https://react.dev" target="_blank">
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </a>
+  </>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Header />
       <h1>Mon Premier Projet React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
