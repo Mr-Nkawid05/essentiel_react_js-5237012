e67b31885a4c63bd84830b71966988482f914959
Sandy <sandy@Sandys-MBP-2.lan>
2025-04-04 16:25:27 +0200

    03_03_f

--- fichiers-exercice/03/03_02/src/App.jsx
+++ fichiers-exercice/03/03_02/src/App.jsx
@@ -5,14 +5,14 @@ import './App.css'
 
 function Header() {
   return (
-  <>
-    <a href="https://vite.dev" target="_blank">
-      <img src={viteLogo} className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://react.dev" target="_blank">
-      <img src={reactLogo} className="logo react" alt="React logo" />
-    </a>
-  </>
+    <>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </>
   )
 }
 
