5ba1a6fc5dbc5d5ea534e6bb6f4ffdddb5c4a657
Sandy <sandy@Sandys-MBP-2.lan>
2025-04-09 13:12:59 +0200

    06_05_f

---
+++ fichiers-exercice/06/06_05/src/WeatherComponent.jsx
@@ -0,0 +1,9 @@
+import { use } from 'react';
+
+export function WeatherComponent({ weatherData }) {
+  if (!weatherData) return <p className="text-info">No Data Available - Check your Internet Connection 🌐</p>
+  return <div className="card mb-5">
+    <p>Temp: {weatherData?.main?.temp}°C</p>
+    <p>Condition: {weatherData?.weather[0].description}</p>
+  </div>
+}
